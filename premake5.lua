85859f5a57de345fe45641351e92315ad3857536
Fylkirist <112450556+Fylkirist@users.noreply.github.com>
2025-11-13 06:44:02 +0100

    premake

---
+++ premake5.lua
@@ -0,0 +1,50 @@
+project "NVRHI"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    flags { "NoPCH" }
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir    ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "src/**.cpp",
+        "src/**.h",
+        "src/**.hpp",
+        "include/**.h",
+        "include/**.hpp"
+    }
+
+    includedirs {
+        "include",
+        "src",
+        "thirdparty",
+        "thirdparty/Vulkan-Headers/include",
+        "thirdparty/DirectX-Headers/include"
+    }
+
+    sysincludedirs {
+        "thirdparty/Vulkan-Headers/include",
+        "thirdparty/DirectX-Headers/include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "Off"
+        defines { "_CRT_SECURE_NO_WARNINGS", "WIN32", "VK_USE_PLATFORM_WIN32_KHR", "NOMINMAX", "WIN32_LEAN_AND_MEAN" }
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+        staticruntime "Off"
+        defines { "LINUX" }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+
+    filter {}
